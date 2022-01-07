6872c5f0a0f8a5fe1e892c342300ba7e6863f8be
VdustR <VdustR@gmail.com>
2022-01-08 03:36:18 +0800

    fix item layout

--- src/list/Item.svelte
+++ src/list/Item.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
   import type { Site } from "@/sites";
+  import OpenOutIcon from "./OpenOutIcon.svelte";
   import Tags from "./Tags.svelte";
   export let site: Site;
 </script>
 
-<li class="inline-flex relative w-full">
+<li
+  class="inline-flex relative w-full py-1.5 flex-col border-width-2 rounded transition-all ease-in-out duration-200 transform bg-black bg-opacity-75 scale-95 hover:scale-100 hover:backdrop-filter hover:backdrop-blur-md gap-2"
+>
   <a
-    class="w-full h-full py-1.5 flex flex-col flex-1 border-width-2 rounded transition-all ease-in-out duration-200 transform bg-black bg-opacity-70 scale-95 hover:bg-emerald-800 hover:bg-opacity-50 hover:text-emerald-200 hover:scale-100 hover:backdrop-filter hover:backdrop-blur-md gap-2"
+    class="px-2 flex flex-col gap-2 hover:text-emerald-200"
     href={site.url}
     target="_blank"
     rel="noopener noreferrer"
   >
-    <h3 class="px-2 text-lg">
-      <a
-        class="text-sm block leading-tight text-emerald-200 mb-1 font-bold"
-        href={site.url}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {site.url}
-      </a>
-      {site.title}
-    </h3>
-    <Tags {site} />
-    <hr />
-    <p class="px-2">
-      {site.description}
-    </p>
+    <div class="text-xs leading-tight flex justify-between items-top gap-2">
+      <div>
+        <h3 class="text-xl font-bold ">
+          {site.title}
+        </h3>
+        <div>{site.url}</div>
+      </div>
+      <div><OpenOutIcon /></div>
+    </div>
   </a>
+  <hr />
+  <Tags {site} />
+  <hr />
+  <p class="px-2">
+    {site.description}
+  </p>
 </li>
