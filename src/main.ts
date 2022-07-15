8f1b5d0b00a201ba05705eb7d2805469daca2ab9
VdustR <vdustr@gmail.com>
2022-07-15 15:58:57 +0800

    fix type

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import "virtual:windi.css";
 import App from "./App.svelte";
 import "./root.css";
 
+const target = document.getElementById("app");
+
+if (!target) {
+  throw new Error("No app element found");
+}
+
 const app = new App({
-  target: document.getElementById("app"),
+  target,
 });
 
 export default app;
